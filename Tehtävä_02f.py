d51e25a48f0f0f5ceb9cefc5657f65c2bb4721a0
Armaan <Armaan.Sharma@metropolia.fi>
2026-01-16 19:59:27 +0200

    Harjoitustehtävä 02f

---
+++ Tehtävä_02f.py
@@ -0,0 +1,5 @@
+import random
+koodi3=f"{random.randint(0,9)}{random.randint(0,9)}{random.randint(0,9)}"
+koodi4=f"{random.randint(1,6)}{random.randint(1,6)}{random.randint(1,6)}{random.randint(1,6)}"
+print(f"Kolmenumeroinen koodi:{koodi3}")
+print(f"Nelinumeroinen koodi:{koodi4}")
