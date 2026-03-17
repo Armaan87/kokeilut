b95df10b67aef44dbe1dbbb65fd3d9f4e854a53a
Armaan <Armaan.Sharma@metropolia.fi>
2026-03-17 22:06:19 +0200

    Harjoitustehtävä 09b

---
+++ Tehtävä_09b.py
@@ -0,0 +1,26 @@
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus = 0
+        self.kuljettu_matka = 0
+
+    def kiihdyta(self, muutos):
+        self.nopeus += muutos
+
+        if self.nopeus > self.huippunopeus:
+            self.nopeus = self.huippunopeus
+        if self.nopeus < 0:
+            self.nopeus = 0
+
+
+auto = Auto("ABC-123", 142)
+
+auto.kiihdyta(30)
+auto.kiihdyta(70)
+auto.kiihdyta(50)
+
+print("Nopeus:", auto.nopeus)
+
+auto.kiihdyta(-200)
+print("Nopeus hätäjarrutuksen jälkeen:", auto.nopeus)
