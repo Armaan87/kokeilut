ade2ce96445f159f975829fce12319cde016abdd
Armaan <Armaan.Sharma@metropolia.fi>
2026-03-21 18:09:27 +0200

    Harjoitustehtävä 10b

---
+++ Tehtävä_10b.py
@@ -0,0 +1,40 @@
+class Hissi:
+    def __init__(self, alin, ylin):
+        self.alin = alin
+        self.ylin = ylin
+        self.kerros = alin
+
+    def kerros_ylos(self):
+        if self.kerros < self.ylin:
+            self.kerros += 1
+            print("Nyt kerroksessa:", self.kerros)
+
+    def kerros_alas(self):
+        if self.kerros > self.alin:
+            self.kerros -= 1
+            print("Nyt kerroksessa:", self.kerros)
+
+    def siirry_kerrokseen(self, kohde):
+        while self.kerros < kohde:
+            self.kerros_ylos()
+        while self.kerros > kohde:
+            self.kerros_alas()
+
+
+class Talo:
+    def __init__(self, alin, ylin, hissien_maara):
+        self.hissit = []
+
+        for i in range(hissien_maara):
+            self.hissit.append(Hissi(alin, ylin))
+
+    def aja_hissia(self, numero, kohde):
+        self.hissit[numero].siirry_kerrokseen(kohde)
+
+
+
+talo = Talo(1, 10, 3)
+
+talo.aja_hissia(0, 5)
+talo.aja_hissia(1, 8)
+talo.aja_hissia(0, 1)
